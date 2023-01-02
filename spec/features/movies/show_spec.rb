aec6fdff2ca96377fb22fd9354c1e60fbacbbd83
Ryan Canton <ryancanton999@gmail.com>
2023-01-02 10:15:31 -0700

    Test: Add model and feature test for user story 2

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,29 @@
+# As a user,
+# When I visit a movie's show page.
+# I see the movie's title, creation year, and genre,
+# and a list of all its actors from youngest to oldest.
+# And I see the average age of all of the movie's actors
+require 'rails_helper'
+
+RSpec.describe "The Movie show page" do
+  it 'Meets the user story requirements commented above' do
+    ghibli = Studio.create!(name: "Studio Ghibli", location: "Japan")
+    hmc = ghibli.movies.create!(title: "Howl's Moving Castle", creation_year: "2004", genre: "Adventure")
+    c_bale = Actor.create!(name: "Christian Bale", age: 47)
+    b_crystal = Actor.create!(name: "Billy Crystal", age: 52)
+    j_hutcherson = Actor.create!(name: "Josh Hutcherson", age: 26)
+    MovieActor.create!(movie_id: hmc.id, actor_id: c_bale.id)
+    MovieActor.create!(movie_id: hmc.id, actor_id: b_crystal.id)
+    MovieActor.create!(movie_id: hmc.id, actor_id: j_hutcherson.id)
+
+    visit "/movies/#{hmc.id}"
+
+    expect(page).to have_content(hmc.name)
+    expect(page).to have_content(hmc.creation_year)
+    expect(page).to have_content(hmc.genre)
+
+    expect(page).to have_content("plinplon")
+
+    expect(page).to have_content("Average Actor Age: #{hmc.actor_age_avg}")
+  end
+end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Movie do
     it {should have_many :movie_actors}
     it {should have_many :actors}
   end
+
+  describe '#actor_age_avg' do
+    it 'returns the average age of all actors in a movie' do
+      ghibli = Studio.create!(name: "Studio Ghibli", location: "Japan")
+      hmc = ghibli.movies.create!(title: "Howl's Moving Castle", creation_year: "2004", genre: "Adventure")
+      c_bale = Actor.create!(name: "Christian Bale", age: 47)
+      b_crystal = Actor.create!(name: "Billy Crystal", age: 52)
+      j_hutcherson = Actor.create!(name: "Josh Hutcherson", age: 26)
+      MovieActor.create!(movie_id: hmc.id, actor_id: c_bale.id)
+      MovieActor.create!(movie_id: hmc.id, actor_id: b_crystal.id)
+      MovieActor.create!(movie_id: hmc.id, actor_id: j_hutcherson.id)
+
+      expect(hmc.actor_age_avg).to eq(41.67)
+    end
+  end
 end
