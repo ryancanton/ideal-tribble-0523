796c58e487c53327026de2e5cf73184e9325c48a
Ryan Canton <ryancanton999@gmail.com>
2023-01-02 09:25:17 -0700

    Test/Feat: test and define many-to-many relationship of actors and movies

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Movie do
+RSpec.describe Actor do
   describe 'relationships' do
-    it {should belong_to :studio}
     it {should have_many :movie_actors}
-    it {should have_many :actors}
+    it {should have_many :movies}
   end
 end
